c5a38ef11990892f9f89637e1236d851c90e70f2
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-22 20:16:31 +0200

    feat #1: create user entity

---
+++ src/main/java/org/gordeser/scanner/dao/entity/User.java
@@ -0,0 +1,30 @@
+package org.gordeser.scanner.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+}
