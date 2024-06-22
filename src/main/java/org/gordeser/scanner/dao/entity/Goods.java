0cb390c8e45d478c4f1c12970972e89122c6dc3b
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-22 20:17:02 +0200

    feat #1: add goods entity

---
+++ src/main/java/org/gordeser/scanner/dao/entity/Goods.java
@@ -0,0 +1,30 @@
+package org.gordeser.scanner.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Goods {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @CreatedDate
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+}
