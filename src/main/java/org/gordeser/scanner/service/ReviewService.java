37effce06f015a8eaed429da96a431c94f1ae452
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-23 19:11:40 +0200

    feat #3: add crud operations in review service

---
+++ src/main/java/org/gordeser/scanner/service/ReviewService.java
@@ -0,0 +1,37 @@
+package org.gordeser.scanner.service;
+
+import lombok.RequiredArgsConstructor;
+import org.gordeser.scanner.dao.entity.Review;
+import org.gordeser.scanner.dao.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public List<Review> findAll() {
+        return reviewRepository.findAll();
+    }
+
+    public Review findById(Long id) {
+        return reviewRepository.findById(id).orElse(null);
+    }
+
+    public Review save(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public Review update(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public void deleteById(Long id) {
+        Review review = findById(id);
+        if (review != null) {
+            reviewRepository.delete(review);
+        }
+    }
+}
