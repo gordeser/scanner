f40b31513e0b0c57e49af2297ed3732042d2e583
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-24 14:29:18 +0200

    feat #4: add data and table annotations

--- src/main/java/org/gordeser/scanner/dao/entity/Image.java
+++ src/main/java/org/gordeser/scanner/dao/entity/Image.java
@@ -1,8 +1,15 @@
 package org.gordeser.scanner.dao.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "image")
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/org/gordeser/scanner/dao/entity/Users.java
+++ src/main/java/org/gordeser/scanner/dao/entity/Users.java
@@ -2,11 +2,14 @@ package org.gordeser.scanner.dao.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Table(name = "users")
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
