38eb6d7a2724cb96674bf927c2bd9ea373b81bdb
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-26 11:32:38 +0200

    fix #5: fix naming in user entity

--- src/main/java/org/gordeser/scanner/dao/entity/Users.java
+++ src/main/java/org/gordeser/scanner/dao/entity/Users.java
@@ -28,10 +28,10 @@ public class Users {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "firstName")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "lastName")
+    @Column(name = "last_name")
     private String lastName;
 
     @OneToMany(mappedBy = "createdBy")
