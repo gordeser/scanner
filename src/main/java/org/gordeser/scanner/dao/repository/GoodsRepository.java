5c449122297b606f2fd5542e20c60251148df95d
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-28 22:24:35 +0200

    feat #11: add find all goods by category to repository

--- src/main/java/org/gordeser/scanner/dao/repository/GoodsRepository.java
+++ src/main/java/org/gordeser/scanner/dao/repository/GoodsRepository.java
@@ -1,5 +1,6 @@
 package org.gordeser.scanner.dao.repository;
 
+import org.gordeser.scanner.dao.entity.Category;
 import org.gordeser.scanner.dao.entity.Goods;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface GoodsRepository extends JpaRepository<Goods, Long> {
     List<Goods> getGoodsByCreatedByUsername(String createdByUsername);
+    List<Goods> findByCategoriesContaining(Category category);
 }
