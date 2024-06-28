aa9b1f8846e0615887c79bd35bff744107f43a8b
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-28 20:15:17 +0200

    feat #11: delete useless create user in facade

--- src/main/java/org/gordeser/scanner/facade/UserFacade.java
+++ src/main/java/org/gordeser/scanner/facade/UserFacade.java
@@ -1,7 +1,6 @@
 package org.gordeser.scanner.facade;
 
 import lombok.RequiredArgsConstructor;
-import org.gordeser.scanner.dao.dto.UserDTO;
 import org.gordeser.scanner.dao.dto.UserUpdateDTO;
 import org.gordeser.scanner.dao.entity.User;
 import org.gordeser.scanner.dao.repository.UserRepository;
@@ -15,15 +14,6 @@ public class UserFacade {
     private final UserService userService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public User createUser(UserDTO userDTO) {
-        User newUser = new User();
-        newUser.setUsername(userDTO.getUsername());
-        newUser.setEmail(userDTO.getEmail());
-        newUser.setPassword(userDTO.getPassword());
-
-        return userService.save(newUser);
-    }
 
     public User updateUser(User currentUser, UserUpdateDTO userUpdateDTO) throws Exception {
         String updateEmail = userUpdateDTO.getEmail();
