b6d6ff4fa1356cecb04fa3547bb2324c67d5b5bf
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-28 19:23:25 +0200

    feat #11: implement update user in facade

--- src/main/java/org/gordeser/scanner/facade/UserFacade.java
+++ src/main/java/org/gordeser/scanner/facade/UserFacade.java
@@ -2,9 +2,11 @@ package org.gordeser.scanner.facade;
 
 import lombok.RequiredArgsConstructor;
 import org.gordeser.scanner.dao.dto.UserDTO;
+import org.gordeser.scanner.dao.dto.UserUpdateDTO;
 import org.gordeser.scanner.dao.entity.User;
 import org.gordeser.scanner.dao.repository.UserRepository;
 import org.gordeser.scanner.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class UserFacade {
     private final UserService userService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User createUser(UserDTO userDTO) {
         User newUser = new User();
@@ -22,22 +25,44 @@ public class UserFacade {
         return userService.save(newUser);
     }
 
-    public User updateUser(Long userId, UserDTO userDTO) throws Exception {
-        User user = userRepository.findById(userId).orElse(null);
-        if (user == null) {
-            throw new Exception("Not found");
+    public User updateUser(User currentUser, UserUpdateDTO userUpdateDTO) throws Exception {
+        String updateEmail = userUpdateDTO.getEmail();
+        if (updateEmail != null) {
+
+            if (userRepository.findByEmail(updateEmail).isPresent() &&
+                    !currentUser.getEmail().equals(updateEmail)) {
+                throw new Exception("Email already exists");
+            }
+
+            currentUser.setEmail(updateEmail);
         }
 
-        user.setUsername(userDTO.getUsername());
-        user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
-        if (userDTO.getFirstName() != null) {
-            user.setFirstName(userDTO.getFirstName());
-        }
-        if (userDTO.getLastName() != null) {
-            user.setLastName(userDTO.getLastName());
+        String updateUsername = userUpdateDTO.getUsername();
+        if (updateUsername != null) {
+
+            if (userRepository.findByUsername(updateUsername).isPresent() &&
+                    !currentUser.getUsername().equals(updateUsername)) {
+                throw new Exception("Username already exists");
+            }
+
+            currentUser.setUsername(updateUsername);
         }
 
-        return userService.update(user);
+        String updatePassword = userUpdateDTO.getPassword();
+        if (updatePassword != null) {
+            currentUser.setPassword(passwordEncoder.encode(updatePassword));
+        }
+
+        String firstName = userUpdateDTO.getFirstName();
+        if (firstName != null) {
+            currentUser.setFirstName(firstName);
+        }
+
+        String lastName = userUpdateDTO.getLastName();
+        if (lastName != null) {
+            currentUser.setLastName(lastName);
+        }
+
+        return userService.update(currentUser);
     }
 }
