9871cbd5c90c182fbbc3230036db4398537b4af1
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-06-28 19:24:26 +0200

    feat #11: implement user update endpoint

--- src/main/java/org/gordeser/scanner/controller/UserController.java
+++ src/main/java/org/gordeser/scanner/controller/UserController.java
@@ -3,8 +3,11 @@ package org.gordeser.scanner.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.gordeser.scanner.dao.dto.UserDTO;
+import org.gordeser.scanner.dao.dto.UserUpdateDTO;
 import org.gordeser.scanner.dao.entity.User;
 import org.gordeser.scanner.facade.UserFacade;
+import org.gordeser.scanner.response.UserUpdateResponse;
+import org.gordeser.scanner.service.JwtService;
 import org.gordeser.scanner.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -21,6 +24,7 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     private final UserFacade userFacade;
+    private final JwtService jwtService;
 
 
     @GetMapping("/me")
@@ -50,10 +54,23 @@ public class UserController {
         return ResponseEntity.ok(newUser);
     }
 
-    @PutMapping("/{userId}")
-    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody @Valid UserDTO userDTO) throws Exception {
-        User updatedUser = userFacade.updateUser(userId, userDTO);
-        return ResponseEntity.ok(updatedUser);
+    @PutMapping
+    public ResponseEntity<UserUpdateResponse> updateUser(@RequestBody UserUpdateDTO userUpdateDTO) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        User updatedUser = userFacade.updateUser(currentUser, userUpdateDTO);
+        String newToken = jwtService.generateToken(updatedUser);
+        Long expiresIn = jwtService.getExpirationTime();
+
+        UserUpdateResponse userUpdateResponse = new UserUpdateResponse();
+        userUpdateResponse.setUsername(updatedUser.getUsername());
+        userUpdateResponse.setEmail(updatedUser.getEmail());
+        userUpdateResponse.setFirstName(updatedUser.getFirstName());
+        userUpdateResponse.setLastName(updatedUser.getLastName());
+        userUpdateResponse.setToken(newToken);
+        userUpdateResponse.setExpiresIn(expiresIn);
+
+        return ResponseEntity.ok(userUpdateResponse);
     }
 
     @DeleteMapping("/{userId}")
