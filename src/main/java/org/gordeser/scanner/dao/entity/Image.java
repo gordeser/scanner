0478acb9bad2b2967019696e83bd72480cc2e152
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-07-03 18:50:14 +0200

    feat #15: add attributes to entities

--- src/main/java/org/gordeser/scanner/dao/entity/Image.java
+++ src/main/java/org/gordeser/scanner/dao/entity/Image.java
@@ -1,9 +1,12 @@
 package org.gordeser.scanner.dao.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -19,10 +22,25 @@ public class Image {
     @Column(name = "path")
     private String path;
 
+    @Column(name = "original_filename")
+    private String originalFilename;
+
     @Column(name = "hash")
     private String hash;
 
+    @Column(name = "upload_date", updatable = false)
+    private LocalDateTime uploadDate;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = Boolean.FALSE;
+
     @ManyToOne
     @JoinColumn(name = "goods_id")
+    @JsonIgnore
     private Goods goods;
+
+    @ManyToOne
+    @JoinColumn(name = "uploaded_by")
+    @JsonIgnore
+    private User uploadedBy;
 }
