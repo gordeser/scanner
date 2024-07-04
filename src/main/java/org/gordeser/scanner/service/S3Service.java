0cc6dfea6ad09e3bfc6a03237affbbc79b71e14e
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-07-04 17:13:29 +0200

    feat #15: filename of image as sha256 of content

--- src/main/java/org/gordeser/scanner/service/S3Service.java
+++ src/main/java/org/gordeser/scanner/service/S3Service.java
@@ -1,9 +1,12 @@
 package org.gordeser.scanner.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
+import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
+import com.google.common.io.ByteSource;
 import com.google.common.io.Files;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 @Service
@@ -28,17 +32,19 @@ public class S3Service {
     }
 
     public String uploadFile(String keyName, MultipartFile file) throws IOException {
-        String sha256HexKeyName = Hashing.sha256().hashString(keyName, StandardCharsets.UTF_8).toString();
+        String hexHashFile = Hashing.sha256().hashBytes(file.getBytes()).toString();
 
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
         PutObjectResult putObjectResult = s3client.putObject(
                 bucketName,
-                sha256HexKeyName + "." + Files.getFileExtension(keyName),
+                hexHashFile + "." + Files.getFileExtension(keyName),
                 file.getInputStream(),
-                null
+                metadata
         );
 
         log.info(putObjectResult.getMetadata().toString());
-        return sha256HexKeyName;
+        return hexHashFile;
     }
 
     public S3Object getFile(String keyName) {
